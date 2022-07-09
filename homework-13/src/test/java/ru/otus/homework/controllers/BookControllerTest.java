32ff87896ea1a7f605aefcfbb908fef212030cc4
Yorisen <yorisen@yandex.ru>
2022-07-09 12:16:45 +0300

    homework-13

---
+++ homework-13/src/test/java/ru/otus/homework/controllers/BookControllerTest.java
@@ -0,0 +1,229 @@
+package ru.otus.homework.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.otus.homework.domain.Book;
+import ru.otus.homework.domain.Builder;
+import ru.otus.homework.service.AuthorService;
+import ru.otus.homework.service.BookService;
+import ru.otus.homework.service.GenreService;
+import ru.otus.homework.service.UserService;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@DisplayName("Контроллер книг должен")
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(BookController.class)
+class BookControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private BookService bookService;
+    @MockBean
+    private AuthorService authorService;
+    @MockBean
+    private GenreService genreService;
+    @MockBean
+    private UserService userService;
+
+    private static final BigDecimal BOOK_ID = BigDecimal.valueOf(1);
+
+    Book book;
+    Optional<Book> bookOptional;
+
+    @BeforeEach
+    public void setup() {
+        book = Builder.buildExistingBook();
+        book.setId(BOOK_ID);
+        bookOptional = Optional.ofNullable(book);
+    }
+
+    @DisplayName("возвращать 200 HTTP статус при получении всех книг пользователем с ролью admin")
+    @WithMockUser(
+            username = "admin",
+            authorities = {"ROLE_ADMIN"}
+    )
+    @Test
+    void shouldReturnHttpStatus200onRequestOfAllBooksByRoleAdmin() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("возвращать 200 HTTP статус при получении книги пользователем с ролью admin")
+    @WithMockUser(
+            username = "admin",
+            authorities = {"ROLE_ADMIN"}
+    )
+    @Test
+    void shouldReturnHttpStatus200OnBookRequestByRoleAdmin() throws Exception {
+        given(bookService.findById(BOOK_ID)).willReturn(bookOptional);
+
+        mockMvc.perform(get("/book/edit?id=" + BOOK_ID))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("возвращать 3xx HTTP статус при обновлении книги пользователем с ролью admin")
+    @WithMockUser(
+            username = "admin",
+            authorities = {"ROLE_ADMIN"}
+    )
+    @Test
+    void shouldReturnHttpStatus3xxOnUpdateBookByRoleAdmin() throws Exception {
+        given(bookService.update(book.getId(),
+                book.getName(),
+                book.getAuthor().getId(),
+                book.getGenre().getId())).willReturn(book);
+
+        mockMvc.perform(post("/book/edit")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content("id=" + book.getId() +
+                                "&name=" + book.getName() +
+                                "&author.id=" + book.getAuthor().getId() +
+                                "&genre.id=" + book.getGenre().getId()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @DisplayName("возвращать 3xx HTTP статус при добавлении книги пользователем с ролью admin")
+    @WithMockUser(
+            username = "admin",
+            authorities = {"ROLE_ADMIN"}
+    )
+    @Test
+    void shouldReturnHttpStatus3xxOnAddingBookByRoleAdmin() throws Exception {
+        given(bookService.insert(book.getName(),
+                book.getAuthor().getId(),
+                book.getGenre().getId())).willReturn(book);
+
+        mockMvc.perform(post("/book/edit")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content("name=" + book.getName() +
+                                "&author.id=" + book.getAuthor().getId() +
+                                "&genre.id=" + book.getGenre().getId()))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @DisplayName("возвращать 200 HTTP статус при запросе формы добавления книги пользователем с ролью admin")
+    @WithMockUser(
+            username = "admin",
+            authorities = {"ROLE_ADMIN"}
+    )
+    @Test
+    void shouldReturnHttpStatus200OnRequestOfAddingBookByRoleAdmin() throws Exception {
+        mockMvc.perform(get("/book/add"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("возвращать 3xx HTTP статус при удалении книги пользователем с ролью admin")
+    @WithMockUser(
+            username = "admin",
+            authorities = {"ROLE_ADMIN"}
+    )
+    @Test
+    void deletePage() throws Exception {
+        mockMvc.perform(get("/book/delete?id=" + BOOK_ID))
+                .andExpect(status().is3xxRedirection());
+    }
+
+    @DisplayName("возвращать 200 HTTP статус при получении всех книг пользователем с ролью user")
+    @WithMockUser(
+            username = "user",
+            authorities = {"ROLE_USER"}
+    )
+    @Test
+    void shouldReturnHttpStatus200onRequestOfAllBooksByRoleUser() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("возвращать 403 HTTP статус при получении книги пользователем с ролью user")
+    @WithMockUser(
+            username = "user",
+            authorities = {"ROLE_USER"}
+    )
+    @Test
+    void shouldReturnHttpStatus403OnBookRequestByRoleUser() throws Exception {
+        given(bookService.findById(BOOK_ID)).willReturn(bookOptional);
+
+        mockMvc.perform(get("/book/edit?id=" + BOOK_ID))
+                .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("возвращать 403 HTTP статус при обновлении книги пользователем с ролью user")
+    @WithMockUser(
+            username = "user",
+            authorities = {"ROLE_USER"}
+    )
+    @Test
+    void shouldReturnHttpStatus403OnUpdateBookByRoleUser() throws Exception {
+        given(bookService.update(book.getId(),
+                book.getName(),
+                book.getAuthor().getId(),
+                book.getGenre().getId())).willReturn(book);
+
+        mockMvc.perform(post("/book/edit")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content("id=" + book.getId() +
+                                "&name=" + book.getName() +
+                                "&author.id=" + book.getAuthor().getId() +
+                                "&genre.id=" + book.getGenre().getId()))
+                .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("возвращать 403 HTTP статус при добавлении книги пользователем с ролью user")
+    @WithMockUser(
+            username = "user",
+            authorities = {"ROLE_USER"}
+    )
+    @Test
+    void shouldReturnHttpStatus403OnAddingBookByRoleUser() throws Exception {
+        given(bookService.insert(book.getName(),
+                book.getAuthor().getId(),
+                book.getGenre().getId())).willReturn(book);
+
+        mockMvc.perform(post("/book/edit")
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                        .content("name=" + book.getName() +
+                                "&author.id=" + book.getAuthor().getId() +
+                                "&genre.id=" + book.getGenre().getId()))
+                .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("возвращать 403 HTTP статус при запросе формы добавления книги пользователем с ролью user")
+    @WithMockUser(
+            username = "user",
+            authorities = {"ROLE_USER"}
+    )
+    @Test
+    void shouldReturnHttpStatus403OnRequestOfAddingBookByRoleUser() throws Exception {
+        mockMvc.perform(get("/book/add"))
+            .andExpect(status().isForbidden());
+    }
+
+    @DisplayName("возвращать 403 HTTP статус при удалении книги пользователем с ролью user")
+    @WithMockUser(
+            username = "user",
+            authorities = {"ROLE_USER"}
+    )
+    @Test
+    void shouldReturnHttpStatus403OnRequestOfDeletingBookByRoleUser() throws Exception {
+        mockMvc.perform(get("/book/delete?id=" + BOOK_ID))
+                .andExpect(status().isForbidden());
+    }
+}
